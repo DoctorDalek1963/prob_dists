266d87a78880e7e9d12f783e7635474767e1efd8
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2022-03-08 14:40:46 +0000

    Initialize project

---
+++ src/prob_dists/__init__.py
@@ -0,0 +1,9 @@
+# prob_dists - Calculate probabilities for distributions
+# Copyright (C) 2022 D. Dyson (DoctorDalek1963)
+
+# This program is licensed under GNU GPLv3, available here:
+# <https://www.gnu.org/licenses/gpl-3.0.html>
+
+"""This is the top-level ``prob_dists`` package, which contains all the subpackages of the project."""
+
+__version__ = '0.0.1-alpha'
---
+++ tests/test_temp.py
@@ -0,0 +1,3 @@
+def test_temp() -> None:
+    """This is just to satisfy the pytest workflow."""
+    assert True
