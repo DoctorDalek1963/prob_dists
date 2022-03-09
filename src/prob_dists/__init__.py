bcde0968a05263e03cc80f81e38d93c22159896e
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2022-03-09 23:29:25 +0000

    Add examples and alias table

--- src/prob_dists/__init__.py
+++ src/prob_dists/__init__.py
@@ -4,7 +4,21 @@
 # This program is licensed under GNU GPLv3, available here:
 # <https://www.gnu.org/licenses/gpl-3.0.html>
 
-"""This is the top-level ``prob_dists`` package, which contains all the subpackages and submodules of the project."""
+"""This is the top-level ``prob_dists`` package, which contains all the subpackages and submodules of the project.
+
+Here's a table of user-friendly alises and the backend classes they refer to:
+
+.. list-table::
+   :widths: 20 70
+   :header-rows: 1
+
+   * - Alias
+     - Class or function name
+   * - P
+     - :func:`prob_dists.distributions.calculate_probability`
+   * - B
+     - :class:`prob_dists.distributions.BinomialDistribution`
+"""
 
 from . import distributions
 from .distributions import calculate_probability, NonsenseError
