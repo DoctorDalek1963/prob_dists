40972553ea1bf9a1c718a1267009ddfc75d2f05a
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2022-03-10 09:57:31 +0000

    Create simple PMF and CDF Poisson tests

--- tests/test_distributions.py
+++ tests/test_distributions.py
@@ -12,7 +12,7 @@ import pytest
 from pytest import approx
 
 import prob_dists as pd
-from prob_dists import P, B
+from prob_dists import P, B, Po
 
 
 def test_binomial_pmf() -> None:
@@ -172,3 +172,71 @@ def test_binomial_calculate() -> None:
 
     with pytest.raises(pd.NonsenseError):
         P(X == 3 < 10)
+
+
+def test_poisson_pmf() -> None:
+    """Test the Poisson distribution PMF."""
+    X = Po(2)
+    Y = Po(12.3)
+    Z = Po(8.362)
+
+    assert X.pmf(0) == approx(0.1353352832)
+    assert X.pmf(1) == approx(0.2706705665)
+    assert X.pmf(2) == approx(0.2706705665)
+    assert X.pmf(3) == approx(0.1804470443)
+    assert X.pmf(4) == approx(0.09022352216)
+    assert X.pmf(5) == approx(0.03608940886)
+    assert X.pmf(10) == approx(3.818985065e-5)
+    assert X.pmf(20) == approx(5.832924198e-14)
+
+    assert Y.pmf(0) == approx(4.551744463e-6)
+    assert Y.pmf(1) == approx(5.59864569e-5)
+    assert Y.pmf(2) == approx(3.443167099e-4)
+    assert Y.pmf(3) == approx(1.411698511e-3)
+    assert Y.pmf(4) == approx(4.34097292e-3)
+    assert Y.pmf(5) == approx(0.01067879338)
+    assert Y.pmf(10) == approx(0.09941821344)
+    assert Y.pmf(20) == approx(0.01175318263)
+
+    assert Z.pmf(0) == approx(2.335767129e-4)
+    assert Z.pmf(1) == approx(1.953168473e-3)
+    assert Z.pmf(2) == approx(8.166197386e-3)
+    assert Z.pmf(3) == approx(0.02276191418)
+    assert Z.pmf(4) == approx(0.04758378159)
+    assert Z.pmf(5) == approx(0.07957911634)
+    assert Z.pmf(10) == approx(0.1075890671)
+    assert Z.pmf(20) == approx(2.682305189e-4)
+
+
+def test_poisson_cdf() -> None:
+    """Test the Poisson distribution CDF."""
+    X = Po(2)
+    Y = Po(12.3)
+    Z = Po(8.362)
+
+    assert X.cdf(0) == approx(0.1353352832)
+    assert X.cdf(1) == approx(0.406005879)
+    assert X.cdf(2) == approx(0.6766764201)
+    assert X.cdf(3) == approx(0.8571234644)
+    assert X.cdf(4) == approx(0.9473469831)
+    assert X.cdf(5) == approx(0.983436392)
+    assert X.cdf(10) == approx(0.9999916918)
+    assert X.cdf(20) == approx(1)
+
+    assert Y.cdf(0) == approx(4.551744463e-6)
+    assert Y.cdf(1) == approx(6.053820136e-5)
+    assert Y.cdf(2) == approx(4.048549113e-4)
+    assert Y.cdf(3) == approx(1.816553422e-3)
+    assert Y.cdf(4) == approx(6.157526342e-3)
+    assert Y.cdf(5) == approx(0.01683631973)
+    assert Y.cdf(10) == approx(0.3165827183)
+    assert Y.cdf(20) == approx(0.9851937561)
+
+    assert Z.cdf(0) == approx(2.335767129e-4)
+    assert Z.cdf(1) == approx(2.186745186e-3)
+    assert Z.cdf(2) == approx(0.01035294257)
+    assert Z.cdf(3) == approx(0.03311485675)
+    assert Z.cdf(4) == approx(0.08069863835)
+    assert Z.cdf(5) == approx(0.1602777547)
+    assert Z.cdf(10) == approx(0.7784049707)
+    assert Z.cdf(20) == approx(0.999830179)
